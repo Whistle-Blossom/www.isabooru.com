3c205a6879d37ed0bdc087a1201c02d0e90152e1
Whistle-Blossom <71654119+Whistle-Blossom@users.noreply.github.com>
2021-01-13 06:04:02 -0500

    Create image.ex

---
+++ image.ex
@@ -0,0 +1,32 @@
+defmodule Camo.Image do
+  def image_url(input) do
+    %{host: host} = URI.parse(input)
+
+    if !host or String.ends_with?(host, cdn_host()) or is_nil(camo_key()) do
+      input
+    else
+      camo_digest = :crypto.hmac(:sha, camo_key(), input) |> Base.encode16(case: :lower)
+
+      camo_uri = %URI{
+        host: camo_host(),
+        path: "/" <> camo_digest,
+        query: URI.encode_query(url: input),
+        scheme: "https"
+      }
+
+      URI.to_string(camo_uri)
+    end
+  end
+
+  defp cdn_host do
+    Application.get_env(:philomena, :cdn_host)
+  end
+
+  defp camo_key do
+    Application.get_env(:philomena, :camo_key)
+  end
+
+  defp camo_host do
+    Application.get_env(:philomena, :camo_host)
+  end
+end
